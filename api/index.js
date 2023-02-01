3a9f809c72334378ba57485399103e5e9a463a6b
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-02-01 14:22:49 -0500

    fetching data from the machine with mqtt

--- api/index.js
+++ api/index.js
@@ -45,30 +45,23 @@
 import fetch from "node-fetch";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import {
-  addNewDevice,
-  getAllDevices,
-  getDeviceData,
-  toggleSwitch,
-} from "./devices.js";
+import dotenv from "dotenv"
+import { addNewDevice, getAllDevices, getDeviceData, toggleSwitch } from "./devices.js";
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-const PORT = 3030;
-app.listen(PORT, () =>
-  console.log(`Listening on http://localhost:${PORT} Jacques...`)
-);
+const PORT = 3031
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`))
 
-app.post("/devices", addNewDevice);
-app.get("/devices", getAllDevices);
-app.get("/fetch", async (req, res) => {
-  toggleSwitch();
-  const result = await getDeviceData();
-  const data = await result.json();
-  res.send(data);
-});
+app.post('/devices', addNewDevice)
+app.get('/devices', getAllDevices)
+app.get('/fetch', async (req, res) => {
+    toggleSwitch()
+    const result = await getDeviceData()
+    const data = await result.json()
+    res.send(data)
+})  
